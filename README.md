0d26f964f45e53a54cb64ed13c8a22075629b910
Jufeng Zhang <zjffun@gmail.com>
2022-10-01 23:28:52 +0800

    chore: use source-map-loader

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ TODO:
 ## [Release Notes](./CHANGELOG.md)
 
 ## [Contribute](./CONTRIBUTING.md)
+
+Fix react instance:
+
+```bash
+rm -rf node_modules/react
+ln -s ../../relation/node_modules/react ./node_modules/react
+```
--- gulpfile.js
+++ gulpfile.js
@@ -39,6 +39,11 @@ const nlsCompileTask = function () {
               loader: "ts-loader",
               exclude: ["/node_modules/"],
             },
+            {
+              test: /\.js$/,
+              enforce: "pre",
+              use: ["source-map-loader"],
+            },
           ],
         },
         resolve: {
